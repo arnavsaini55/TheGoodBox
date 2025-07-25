d1b1ac9ff4d2778c575e2ee81a59ce7df88f00f6
arnavsaini55 <arnavsaini.saini123@gmail.com>
2025-07-26 01:18:29 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+const App =() => {
+  return 
+  <SafeAreaView>
+    <Text>
+      Hello WOrld
+    </Text>
+  </SafeAreaView>
+}
+
+
+
+
+export default App;
---
+++ assets/fonts/helper.js
@@ -0,0 +1,26 @@
+export const getFontFamily = (baseFont = 'Inter_18pt-BoldItalic', weight)=>{
+    switch (weight){
+        case '100':
+            return '${baseFont}-Thin';
+              case '200':
+            return '${baseFont}-ExtraThin';
+              case '300':
+            return '${baseFont}-Light';
+              case '400':
+                case 'normal':
+                    return '${baseFont}-Regular';
+              case '500':
+            return '${baseFont}-Medium';
+              case '600':
+            return '${baseFont}-Bold';
+              case '700':
+                case 'bold':
+            return '${baseFont}-Bold';
+              case '800':
+            return '${baseFont}-ExtraBold';
+              case '900':
+            return '${baseFont}-SemiBold';
+            default:
+                return'${baseFont}-Regular';
+            }
+}
